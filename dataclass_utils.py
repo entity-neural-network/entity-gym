7a447b2f85a83b699f8733e59c8929a36e80a63a
Clemens Winter <clemenswinter1@gmail.com>
2021-11-21 13:26:09 -0800

    Generate state_space and observations from dataclasses

---
+++ dataclass_utils.py
@@ -0,0 +1,31 @@
+from typing import Any, List, Dict, Type
+from dataclasses import is_dataclass
+import numpy as np
+
+from .environment import (
+    Entity,
+    ObsFilter,
+)
+
+
+def state_space_from_dataclasses(*dss: Type) -> Dict[str, Entity]:
+    state_space = {}
+    for ds in dss:
+        if not is_dataclass(ds):
+            raise ValueError(f"{ds} is not a dataclass")
+        # TODO: check field types are valid
+        state_space[ds.__name__] = Entity(
+            features=list(ds.__dataclass_fields__.keys()),
+        )
+    return state_space
+
+
+def extract_features(
+    entities: Dict[str, List[Any]], obs_filter: ObsFilter
+) -> Dict[str, np.ndarray]:
+    selectors = {}
+    for entity_name, features in obs_filter.entity_to_feats.items():
+        selectors[entity_name] = np.array(
+            [[getattr(e, f) for f in features] for e in entities[entity_name]],
+        )
+    return selectors
